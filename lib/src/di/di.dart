f760ed5ee1ef178aecd82f499bbda3901847c3a8
Anders Cheow <cheowheekeong@hotmail.com>
2021-10-27 11:12:39 +0800

    Use built-in Dio logging interceptor

--- lib/src/di/di.dart
+++ lib/src/di/di.dart
@@ -3,7 +3,6 @@ import 'package:flutter_template/src/api/api_client.dart';
 import 'package:flutter_template/src/constant/config.dart';
 import 'package:flutter_template/src/repositories/auth/auth_repository.dart';
 import 'package:flutter_template/src/repositories/settings/settings_repository.dart';
-import 'package:flutter_template/src/utils/dio_logger_interceptor.dart';
 import 'package:kiwi/kiwi.dart';
 
 void setupDI(IConfig config) {
@@ -12,14 +11,12 @@ void setupDI(IConfig config) {
   container.registerInstance(config);
 
   Dio dio = Dio()
-    ..interceptors.add(DioLoggerInterceptor(
+    ..interceptors.add(LogInterceptor(
       requestHeader: true,
       requestBody: true,
       responseBody: true,
       responseHeader: false,
       error: true,
-      compact: true,
-      maxWidth: 90,
     ));
   ApiClient apiClient = ApiClient(dio, baseUrl: config.getBaseUrl());
   container.registerInstance(apiClient);
